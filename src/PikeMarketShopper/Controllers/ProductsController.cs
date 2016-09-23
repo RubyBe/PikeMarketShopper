dca0ed0266ba58454788aca3c090cb464396581e
RubyBe <sidbenavente@hotmail.com>
2016-09-23 14:18:38 -0700

    Add basic additional crud functionality for product model

--- src/PikeMarketShopper/Controllers/ProductsController.cs
+++ src/PikeMarketShopper/Controllers/ProductsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Dynamic;
 using System.Linq;
 using PikeMarketShopper.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace PikeMarketShopper.Controllers
 {
@@ -10,12 +12,55 @@ namespace PikeMarketShopper.Controllers
     PikeMarketDbContext db = new PikeMarketDbContext();
     public IActionResult Index()
     {
-      dynamic duoModel = new ExpandoObject();
+      dynamic duoModelIndex = new ExpandoObject();
       var theseProducts = db.Products.ToList();
       var theseProductTypes = db.ProductTypes.ToList();
-      duoModel.Products = theseProducts;
-      duoModel.ProductTypes =theseProductTypes;
-      return View(duoModel);
+      duoModelIndex.Products = theseProducts;
+      duoModelIndex.ProductTypes =theseProductTypes;
+      return View(duoModelIndex);
     }
+    // Get and Post for Creating a new product
+    public IActionResult Create()
+    {
+      ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "Name");
+      return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+      db.Products.Add(product);
+      db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    // Get and Post for Editing an existing product
+    public IActionResult Edit(int id)
+    {
+      ViewBag.ProductTypeId = new SelectList(db.ProductTypes, "ProductTypeId", "Name");
+      var thisProduct = db.Products.FirstOrDefault(product => product.ProductId == id);
+      return View(thisProduct);
+    }
+    [HttpPost]
+    public IActionResult Edit(Product product)
+    {
+      db.Entry(product).State = EntityState.Modified;
+      db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    //Post for Confirming Delete and Deleting an existing product
+    public IActionResult Delete(int id)
+    {
+      var thisProduct = db.Products.FirstOrDefault(product => product.ProductId == id);
+      return View(thisProduct);
+    }
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+      var thisProduct = db.Products.FirstOrDefault(product => product.ProductId == id);
+      db.Remove(thisProduct);
+      db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
