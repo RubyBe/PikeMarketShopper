0668c78f47691849634d496bdedab5c07b2beca2
RubyBe <sidbenavente@hotmail.com>
2016-09-25 11:02:02 -0700

    Add user management display

---
+++ src/PikeMarketShopper/Controllers/RolesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace PikeMarketShopper.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
