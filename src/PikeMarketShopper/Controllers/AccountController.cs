76a2f19cfc54c06eb170df2fc69f8f1cfb5f2768
RubyBe <sidbenavente@hotmail.com>
2016-09-25 08:41:38 -0700

    Add login functionality

--- src/PikeMarketShopper/Controllers/AccountController.cs
+++ src/PikeMarketShopper/Controllers/AccountController.cs
@@ -49,6 +49,18 @@ namespace PikeMarketShopper.Controllers
     {
       return View();
     }
-
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
