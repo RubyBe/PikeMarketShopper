dc06d926c024dea697fee37b6f44f56f6004d441
RubyBe <sidbenavente@hotmail.com>
2016-09-28 10:58:14 -0700

    Add product controller view tests

---
+++ PikeMarketShopper.Tests/ControllerTests/ProductsControllerViewTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PikeMarketShopper.Controllers;
+using PikeMarketShopper.Models;
+using Xunit;
+
+namespace PikeMarketShopper.Tests.ControllerTests
+{
+  public class ProductsControllerViewTests
+  {
+    [Fact]
+    public void Get_ViewResult_Create_Test()
+    {
+      // Arrange
+      ProductsController controller = new ProductsController();
+      // Act
+      var result = controller.Create();
+      // Assert
+      Assert.IsType<ViewResult>(result);
+    }
+    public void Get_ViewResult_Details_Test()
+    {
+      // Arrange
+      ProductsController controller = new ProductsController();
+      // Act
+      var result = controller.Details(1);
+      // Assert
+      Assert.IsType<ViewResult>(result);
+    }
+    public void Get_ViewResult_Edit_Test()
+    {
+      // Arrange
+      ProductsController controller = new ProductsController();
+      // Act
+      var result = controller.Edit(1);
+      // Assert
+      Assert.IsType<ViewResult>(result);
+    }
+    public void Get_ViewResult_Delete_Test()
+    {
+      // Arrange
+      ProductsController controller = new ProductsController();
+      // Act
+      var result = controller.Delete(1);
+      // Assert
+      Assert.IsType<ViewResult>(result);
+    }
+    public void Get_ViewResult_Index_Test()
+    {
+      // Arrange
+      ProductsController controller = new ProductsController();
+      // Act
+      var result = controller.Index();
+      // Assert
+      Assert.IsType<ViewResult>(result);
+    }
+  }
+}
