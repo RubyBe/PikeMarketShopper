e002d1967bdf9aeb2ba7dd6b96736d14a469fa73
RubyBe <sidbenavente@hotmail.com>
2016-10-03 16:56:34 -0700

    Add new mock database files

---
+++ src/PikeMarketShopper/Models/Repositories/EFProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PikeMarketShopper.Models.Repositories
+{
+  public class EFProductRepository : IProductRepository
+  {
+    PikeMarketDbContext db = new PikeMarketDbContext();
+
+    public IQueryable<Product> Products { get { return db.Products; } }
+    public Product Save(Product product)
+    {
+      db.Products.Add(product);
+      db.SaveChanges();
+      return product;
+    }
+    public Product Edit(Product product)
+    {
+      db.Entry(product).State = EntityState.Modified;
+      db.SaveChanges();
+      return product;
+    }
+
+    public void Remove(Product product)
+    {
+      db.Products.Remove(product);
+      db.SaveChanges();
+    }
+  }
+}
