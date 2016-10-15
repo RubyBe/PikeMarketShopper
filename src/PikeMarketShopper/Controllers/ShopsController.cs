5ac945ae419a70aa703db955e4c6af391f5daca8
RubyBe <sidbenavente@hotmail.com>
2016-10-15 08:50:38 -0700

    Add start to shops section.

--- src/PikeMarketShopper/Controllers/ShopsController.cs
+++ src/PikeMarketShopper/Controllers/ShopsController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using System.Linq;
+
 using PikeMarketShopper.Models;
 
 namespace PikeMarketShopper.Controllers
 {
+  [AllowAnonymous]
   public class ShopsController : Controller
   {
     PikeMarketDbContext db = new PikeMarketDbContext();
